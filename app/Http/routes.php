5024d86db5e4440e4cf3ad381402fcab27c98fed
dmittal1 <mittald@hotmail.co.uk>
2017-12-20 09:47:07 +0000

    Code Done

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,49 @@
 |
 */
 
+use App\User;
+use App\Post;
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Route::get('/create',function(){
+//
+//   $user = User::findOrFail(1);
+//
+//   //$post = new Post(['title'=>'My first post with Edwin Diaz','body'=>'I love Laravel, with Edwin Diaz']);
+//
+//   $user->posts()->save(new Post(['title'=>'My first post with Edwin Diaz 2','body'=>'I love Laravel, with Edwin Diaz 2']));
+//
+// });
+//
+// Route::get('/read', function(){
+//
+//   $user = User::findOrFail(1);
+//
+//   foreach($user->posts as $post){
+//
+//     echo $post->title . "<br>";
+//
+//   }
+//
+// });
+
+Route::get('/update',function(){
+
+  $user = User::find(1);
+
+//  $user->posts()->whereId(1)->update(['title'=>'I love laravel','body'=>'This is awesome, thank you Ediwn']);
+
+   $user->posts()->where('id','=',2)->update(['title'=>'I love laravel 2','body'=>'This is awesome, thank you Ediwn 2']);
+});
+
+Route::get('/delete', function(){
+
+  $user = User::find(1);
+
+  //$user->posts()->whereId(1)->delete();
+  $user->posts()->delete();
+
 });
